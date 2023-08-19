f72745a65a289e16b89462cfab0b02e0c354a1d1
Longlife_Hyun <44224844+LonglifeHyun@users.noreply.github.com>
2023-08-19 16:15:31 +0900

    [unrated] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -13,15 +13,24 @@
 # ORDER BY C.CAR_ID DESC
 
 # SOL2) SG
-SELECT  DISTINCT CAR_ID,
-        CASE
-            WHEN EXISTS(SELECT  *
-                        FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY B
-                        WHERE   1=1
-                        AND     A.CAR_ID = B.CAR_ID
-                        AND     '2022-10-16' BETWEEN B.START_DATE AND B.END_DATE)
-            THEN '대여중'
-            ELSE '대여 가능'
-        END AS AVAILABILITY
-FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY A
-ORDER BY CAR_ID DESC;
+# SELECT  DISTINCT CAR_ID,
+#         CASE
+#             WHEN EXISTS(SELECT  *
+#                         FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY B
+#                         WHERE   1=1
+#                         AND     A.CAR_ID = B.CAR_ID
+#                         AND     '2022-10-16' BETWEEN B.START_DATE AND B.END_DATE)
+#             THEN '대여중'
+#             ELSE '대여 가능'
+#         END AS AVAILABILITY
+# FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY A
+# ORDER BY CAR_ID DESC;
+
+
+# SOL 3) L-cloud
+select A.car_id AS CAR_ID, CASE WHEN max(A.b) = 1 THEN '대여중' ELSE '대여 가능' END as AVAILABILITY
+from (select CAR_ID, CASE WHEN START_DATE <= '2022-10-16' AND '2022-10-16' <= END_DATE Then 1 else 0 END as b
+from car_rental_company_rental_history
+group by car_id, b) AS A
+group by A.car_id
+ORDER BY 1 DESC
