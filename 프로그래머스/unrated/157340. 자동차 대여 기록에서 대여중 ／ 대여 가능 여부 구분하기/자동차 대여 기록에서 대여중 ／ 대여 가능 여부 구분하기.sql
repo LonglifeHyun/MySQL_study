71a5f5fbb16e43222282d6c0db9fd4f569986859
Longlife_Hyun <44224844+LonglifeHyun@users.noreply.github.com>
2023-08-19 16:00:06 +0900

    [unrated] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,12 +1,27 @@
 -- 코드를 입력하세요
 
-SELECT  C.CAR_ID,
-        IF( ((COUNT(1) = 1) AND (C.AVAILABILITY = '대여 가능')), '대여 가능', '대여중') AS AVAILABILITY
-FROM (  SELECT B.CAR_ID, B.AVAILABILITY, COUNT(1) AS CNT 
-        FROM (  SELECT  A.*, 
-                        IF('2022-10-16' BETWEEN DATE_FORMAT(START_DATE, '%Y-%m-%d') AND DATE_FORMAT(END_DATE, '%Y-%m-%d'), 
-                           '대여중', '대여 가능') AS AVAILABILITY
-                FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY A) B
-        GROUP BY B.CAR_ID, B.AVAILABILITY ) C
-GROUP BY C.CAR_ID
-ORDER BY C.CAR_ID DESC
+# SOL1) MY
+# SELECT  C.CAR_ID,
+#         IF( ((COUNT(1) = 1) AND (C.AVAILABILITY = '대여 가능')), '대여 가능', '대여중') AS AVAILABILITY
+# FROM (  SELECT B.CAR_ID, B.AVAILABILITY, COUNT(1) AS CNT 
+#         FROM (  SELECT  A.*, 
+#                         IF('2022-10-16' BETWEEN DATE_FORMAT(START_DATE, '%Y-%m-%d') AND DATE_FORMAT(END_DATE, '%Y-%m-%d'), 
+#                            '대여중', '대여 가능') AS AVAILABILITY
+#                 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY A) B
+#         GROUP BY B.CAR_ID, B.AVAILABILITY ) C
+# GROUP BY C.CAR_ID
+# ORDER BY C.CAR_ID DESC
+
+# SOL2) SG
+SELECT  DISTINCT CAR_ID,
+        CASE
+            WHEN EXISTS(SELECT  *
+                        FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY B
+                        WHERE   1=1
+                        AND     A.CAR_ID = B.CAR_ID
+                        AND     '2022-10-16' BETWEEN B.START_DATE AND B.END_DATE)
+            THEN '대여중'
+            ELSE '대여 가능'
+        END AS AVAILABILITY
+FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY A
+ORDER BY CAR_ID DESC;
